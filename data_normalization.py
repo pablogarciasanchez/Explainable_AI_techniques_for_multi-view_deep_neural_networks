5e73956c8b4ea23a1010c919881796f63c4332cb
Pablo García Sánchez <147088832+pablogarciasanchez@users.noreply.github.com>
2023-10-18 11:41:25 +0200

    Update data_normalization.py

--- data_normalization.py
+++ data_normalization.py
@@ -14,9 +14,6 @@ train = pd.read_csv('./train.csv')
 train_list_rgb = fn.get_images_list(train,'rgb')
 norm_params_rgb_mean, norm_params_rgb_std = fn.load_image_normalize_rgb(train_list_rgb, "./data_img/", (36,108))
 
-mean = [0.09538473933935165, 0.06967461854219437, 0.04578746110200882]
-std = [0.20621216297149658, 0.15044525265693665, 0.088300921022892]
-
 with open('valores_mean_std.txt', 'w') as f:
-    f.write(",".join(map(str, mean)) + "\n")
-    f.write(",".join(map(str, std)) + "\n")
+    f.write(",".join(map(str, norm_params_rgb_mean)) + "\n")
+    f.write(",".join(map(str, norm_params_rgb_std)) + "\n")
