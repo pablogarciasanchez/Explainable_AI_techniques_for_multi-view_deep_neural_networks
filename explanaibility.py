e7c1cda59524560f7186655881918a3cb0090b8d
Pablo García Sánchez <147088832+pablogarciasanchez@users.noreply.github.com>
2023-10-18 14:01:34 +0200

    Update explanaibility.py

--- explanaibility.py
+++ explanaibility.py
@@ -15,10 +15,10 @@ import functions as fn
 import cv2
 from torchinfo import summary
 import sys
-import torchvision.models as models
 from captum.attr import IntegratedGradients
 from captum.attr import NoiseTunnel
 from captum.attr import Saliency
+from captum.attr import LayerActivation
 sys.stdout.reconfigure(encoding='utf-8')
 
 from utils import PanoramaCNN, ResnetCNN, PanoramaCNNEx, ResnetCNNEx, CustomImageDataset
@@ -36,28 +36,32 @@ def apply_color_map_to_data(data):
             colored_data[i, j] = custom_color_map(data[i, j])
     return colored_data
 
-def overlay_heatmap_on_image(heatmap, original_image, alpha=0.5):
+def overlay_heatmap_on_image(heatmap_red, heatmap_blue, original_image, alpha=0.5):
+    
+    # Cambiar el tamaño de los mapas de calor para que coincidan con el de la imagen original
+    heatmap_red_resized = cv2.resize(heatmap_red, (original_image.width, original_image.height))
+    heatmap_blue_resized = cv2.resize(heatmap_blue, (original_image.width, original_image.height))
+    
+    # Crear mapas de calor en rojo y azul
+    heatmap_red_rgb = np.zeros((original_image.height, original_image.width, 3), dtype=np.uint8)
+    heatmap_red_rgb[:, :, 0] = heatmap_red_resized  # Canal rojo
 
-    # Normalizar el heatmap para que los valores estén entre -1 y 1
-    heatmap_normalized = (heatmap - np.min(heatmap)) / (np.max(heatmap) - np.min(heatmap)) * 2 - 1
-    
-    # Cambiar el tamaño del mapa de calor para que coincida con el de la imagen original
-    heatmap_resized = cv2.resize(heatmap_normalized, (original_image.width, original_image.height))
-    
-    # Aplicar el mapeo de colores personalizado
-    heatmap_colored = apply_color_map_to_data(heatmap_resized)
-    
-    # Convertir la imagen PIL a array numpy y convertir a 3 canales (color)
-    original_image_array = np.array(original_image)
-    original_image_colored = cv2.cvtColor(original_image_array, cv2.COLOR_GRAY2RGB)
-    
-    # Superponer el mapa de calor en la imagen original
-    superimposed_image = cv2.addWeighted(original_image_colored, 1 - alpha, heatmap_colored, alpha, 0)
-    
-    # Convertir el array numpy de la imagen superpuesta a formato PIL
-    superimposed_image_pil = Image.fromarray(superimposed_image)
+    heatmap_blue_rgb = np.zeros((original_image.height, original_image.width, 3), dtype=np.uint8)
+    heatmap_blue_rgb[:, :, 2] = heatmap_blue_resized  # Canal azul
 
-    return superimposed_image_pil
+    # Fusionar los mapas de calor rojo y azul
+    combined_heatmap = cv2.addWeighted(heatmap_red_rgb, 1, heatmap_blue_rgb, 1, 0)
+
+    # Convertir la imagen PIL a un array de numpy y asegurarse de que sea RGB
+    original_rgb = np.array(original_image)
+    if len(original_rgb.shape) == 2:  # Si es escala de grises
+        original_rgb = cv2.cvtColor(original_rgb, cv2.COLOR_GRAY2RGB)
+
+    # Fusionar el mapa de calor combinado y la imagen original
+    fusionada = cv2.addWeighted(original_rgb, 1 - alpha, combined_heatmap, alpha, 0)
+
+    # Convertir el arreglo de NumPy de nuevo a imagen PIL y retornarlo
+    return Image.fromarray(fusionada).resize((224, 224))
 
 def overlay_heatmap_on_image_gradcam(heatmap, original_image, alpha=0.5):
 
@@ -77,27 +81,18 @@ def overlay_heatmap_on_image_gradcam(heatmap, original_image, alpha=0.5):
     fusionada = cv2.addWeighted(original_rgb, 1 - alpha, heatmap_colored_rgb, alpha, 0)
 
     # Convertir el arreglo de NumPy de nuevo a imagen PIL y retornarlo
-    return Image.fromarray(fusionada)
+    return Image.fromarray(fusionada).resize((224, 224))
 
 # Funciones auxiliares
 def load_image(image_path, preprocess):
     """Carga y preprocesa una imagen."""
     image = Image.open(image_path)
     # Dividir la imagen en canales
-    r, g, b = image.split()
+    #r, g, b = image.split()
 
-    return g,preprocess(image)
+    gray_image = image.convert('L')
 
-def get_gradcam(images, target_layer, model):
-    """Genera Grad-CAM atribuciones para imágenes."""
-    
-    model.zero_grad()
-    grad_cam = LayerGradCam(model, target_layer)
-    attributions = grad_cam.attribute(images, relu_attributions=True)
-
-    upsampled_attr = LayerAttribution.interpolate(attributions, (36, 108))
-
-    return upsampled_attr
+    return gray_image,preprocess(image)
 
 def get_attribution_channel(attribution, channel):
     return attribution[channel].detach().cpu().numpy()
@@ -124,23 +119,46 @@ def test(model):
     fn.show_stats(true,pred,'mse')
 
 def save_heatmap(output_dir, subfolder, name, data):
-    heatmap_path = os.path.join(output_dir, subfolder,name)
+    # Redimensionar la imagen a 36x108
+    resized_data = cv2.resize(data, (108, 36))
+
+    heatmap_path = os.path.join(output_dir, subfolder, name)
     os.makedirs(os.path.dirname(heatmap_path), exist_ok=True)
-    cv2.imwrite(heatmap_path, data)
+    cv2.imwrite(heatmap_path, resized_data)
 
 def normalized(channel_data):
     # Normalizar el canal de datos usando los valores mínimos y máximos encontrados
-    norm_data = 255 * (channel_data - channel_data.min()) / (channel_data.max() - channel_data.min())
-    norm_data = norm_data.astype(np.uint8)
+    if channel_data.max() != channel_data.min():
+        norm_data = 255 * (channel_data - channel_data.min()) / (channel_data.max() - channel_data.min())
+        norm_data = norm_data.astype(np.uint8)
+    else:
+        # Si max es igual a min, entonces todos los valores en channel_data son iguales.
+        # Puedes decidir cómo manejar este caso. Una opción es simplemente asignar cero a todo:
+        norm_data = np.zeros_like(channel_data, dtype=np.uint8)
     return norm_data
+
+def normalized_2(channel_data1, channel_data2):
+
+    maximo = np.max(np.concatenate((channel_data1, channel_data2)))
+    minimo = np.min(np.concatenate((channel_data1, channel_data2)))
+
+    # Normalizar el canal de datos usando los valores mínimos y máximos encontrados
+    norm_data_pos = 255 * ((channel_data1 - minimo) / (maximo - minimo))
+    norm_data_pos = norm_data_pos.astype(np.uint8)
+
+    # Normalizar el canal de datos usando los valores mínimos y máximos encontrados
+    norm_data_neg = 255 * ((channel_data2 - minimo) / (maximo - minimo))
+    norm_data_neg = norm_data_neg.astype(np.uint8)
+    
+    return norm_data_pos, norm_data_neg
 
 def apply_smoothgrad(images, model, subfolder, image_wo_p):
     ig = IntegratedGradients(model)
+
     noise_tunnel = NoiseTunnel(ig)
 
-    attribution, delta = noise_tunnel.attribute(images, nt_type='smoothgrad', nt_samples=50, nt_samples_batch_size=1, stdevs=0.1,
-                                                     return_convergence_delta=True, n_steps=150)
-    
+    attribution, delta = noise_tunnel.attribute(images, nt_type='smoothgrad', nt_samples=40, nt_samples_batch_size=1, stdevs=0.15,
+                                                     return_convergence_delta=True, n_steps=200)
     attribution = attribution.squeeze()
 
     channels = ['X', 'Y', 'Z']
@@ -148,17 +166,34 @@ def apply_smoothgrad(images, model, subfolder, image_wo_p):
 
     attributions = {channel: get_attribution_channel(attribution, idx)[1] for idx, channel in enumerate(channels)}
 
-    for channel_name, channel_data in attributions.items():
-        i = channel_to_index[channel_name]  # Obtiene el valor de i según channel_name
+    attributions_pos = {}  # Diccionario para almacenar las atribuciones positivas
+    attributions_neg = {}  # Diccionario para almacenar las atribuciones negativas
+    
+    for idx, channel in enumerate(channels):
+        attr_channel = get_attribution_channel(attribution, idx)[1]
+        pos, neg = separate_attributions(attr_channel)
+        attributions_pos[channel] = pos
+        attributions_neg[channel] = neg
 
-        normalized_data = normalized(channel_data)
-        heatmap_img = overlay_heatmap_on_image(normalized_data, images_wo_p[i], 0.6)
-        save_heatmap(output_dir, subfolder, f'Smoothgrad_cnn{channel_name}.png', normalized_data)
-        heatmap_img.save(os.path.join(output_dir, subfolder,f'Smoothgrad_{channel_name}_heatmap.png'))
+    for (channel_name_pos, channel_data_pos), (channel_name_neg, channel_data_neg)  in zip(attributions_pos.items(),attributions_neg.items()):
+
+        i = channel_to_index[channel_name_pos]  # Obtiene el valor de i según channel_name
+
+        normalized_data_pos, normalized_data_neg = normalized_2(channel_data_pos, channel_data_neg)
+        heatmap_img = overlay_heatmap_on_image(normalized_data_pos, normalized_data_neg, images_wo_p[i], 0.5)
+        normalized_data = np.clip(normalized_data_pos + normalized_data_neg + 100, 0, 255)
+        save_heatmap(output_dir, subfolder, f'Smoothgrad_cnn{channel_name_pos}.png', normalized_data)
+        heatmap_img.save(os.path.join(output_dir, subfolder,f'Smoothgrad_{channel_name_pos}_heatmap.png'))
+
+def separate_attributions(attr):
+    positive_attr = np.maximum(0, attr)  # Mantén solo los valores positivos
+    negative_attr = np.abs(np.minimum(0, attr))  # Toma el valor absoluto de los valores negativos
+    return positive_attr, negative_attr
 
 def apply_integratedgradients(images, model, subfolder, image_wo_p):
     ig = IntegratedGradients(model)
-    attribution,delta = ig.attribute(images,return_convergence_delta=True,n_steps=200)
+
+    attribution,delta = ig.attribute(inputs= images, return_convergence_delta=True,n_steps=200)
         
     attribution = attribution.squeeze()
 
@@ -167,33 +202,36 @@ def apply_integratedgradients(images, model, subfolder, image_wo_p):
 
     attributions = {channel: get_attribution_channel(attribution, idx)[1] for idx, channel in enumerate(channels)}
 
-    for channel_name, channel_data in attributions.items():
-        i = channel_to_index[channel_name]  # Obtiene el valor de i según channel_name
+    attributions_pos = {}  # Diccionario para almacenar las atribuciones positivas
+    attributions_neg = {}  # Diccionario para almacenar las atribuciones negativas
+    
+    for idx, channel in enumerate(channels):
+        attr_channel = get_attribution_channel(attribution, idx)[1]
+        pos, neg = separate_attributions(attr_channel)
+        attributions_pos[channel] = pos
+        attributions_neg[channel] = neg
 
-        normalized_data = normalized(channel_data)
-        heatmap_img = overlay_heatmap_on_image(normalized_data, images_wo_p[i], 0.6)
-        save_heatmap(output_dir, subfolder, f'IntegratedGradients_cnn{channel_name}.png', normalized_data)
-        heatmap_img.save(os.path.join(output_dir, subfolder,f'IntegratedGradients_{channel_name}_heatmap.png'))
+    for (channel_name_pos, channel_data_pos), (channel_name_neg, channel_data_neg)  in zip(attributions_pos.items(),attributions_neg.items()):
 
-def apply_saliency(images, model, subfolder, image_wo_p):
-    saliency = Saliency(model)
-    attribution = saliency.attribute(images, abs=False)
-        
-    attribution = attribution.squeeze()
+        i = channel_to_index[channel_name_pos]  # Obtiene el valor de i según channel_name
 
-    channels = ['X', 'Y', 'Z']
-    channel_to_index = {'X': 0, 'Y': 1, 'Z': 2}  # Mapeo de canales a índices
+        normalized_data_pos, normalized_data_neg = normalized_2(channel_data_pos, channel_data_neg)
+        heatmap_img = overlay_heatmap_on_image(normalized_data_pos, normalized_data_neg, images_wo_p[i], 0.5)
+        normalized_data = np.clip(normalized_data_pos + normalized_data_neg + 100, 0, 255)
+        save_heatmap(output_dir, subfolder, f'IntegratedGradients_cnn{channel_name_pos}.png', normalized_data)
+        heatmap_img.save(os.path.join(output_dir, subfolder,f'IntegratedGradients_{channel_name_pos}_heatmap.png'))
 
-    attributions = {channel: get_attribution_channel(attribution, idx)[1] for idx, channel in enumerate(channels)}
+def get_gradcam(images, target_layer, model):
+    """Genera Grad-CAM atribuciones para imágenes."""
+    
+    model.zero_grad()
+    grad_cam = LayerGradCam(model, target_layer)
+    
+    attributions = grad_cam.attribute(images, relu_attributions= False)
 
-    for channel_name, channel_data in attributions.items():
-        i = channel_to_index[channel_name]  # Obtiene el valor de i según channel_name
+    attributions = torch.abs(attributions)
 
-        normalized_data = normalized(channel_data)
-        heatmap_img = overlay_heatmap_on_image(normalized_data, image_wo_p[i], 0.6)
-        save_heatmap(output_dir, subfolder, f'Saliency_cnn{channel_name}.png', normalized_data)
-        heatmap_img.save(os.path.join(output_dir, subfolder, f'Saliency_{channel_name}_heatmap.png'))
-
+    return attributions
 
 def apply_gradcam(images, model, is_panorama, subfolder, image_wo_p):
     cnn_names = ["cnnX", "cnnY", "cnnZ"]
@@ -203,22 +241,40 @@ def apply_gradcam(images, model, is_panorama, subfolder, image_wo_p):
         else:
             target_layer = model.__getattr__(cnn)[0][-1][-1].conv3
 
+        #print(target_layer)
         attributions = get_gradcam(images, target_layer, model)
         
         attributions = attributions.squeeze(0, 1).detach().cpu().numpy()
         
         # Normalizar el canal de datos usando los valores mínimos y máximos encontrados
         normalized_data = normalized(attributions)
-        heatmap_img = overlay_heatmap_on_image_gradcam(normalized_data, images_wo_p[i], 0.6)
+        heatmap_img = overlay_heatmap_on_image_gradcam(normalized_data, image_wo_p[i], 0.4)
         save_heatmap(output_dir, subfolder, f'GradCam_{cnn}.png', normalized_data)
         heatmap_img.save(os.path.join(output_dir, subfolder,f'GradCam_{cnn}_heatmap.png'))
-    
-# PARÁMETROS
-PANORAMACNN = True
-Explainable = True
 
-mean_train = [0.09538473933935165, 0.06967461854219437, 0.04578746110200882]
-std_train = [0.20621216297149658, 0.15044525265693665, 0.088300921022892]
+def str_to_bool(s):
+    if s == 'True':
+        return True
+    elif s == 'False':
+        return False
+    else:
+        raise ValueError(f"Se esperaba 'True' o 'False', pero se recibió: {s}")
+
+if len(sys.argv) != 4:
+    print("Uso: explanaibility.py [PANORAMACNN:True/False] [Explainable:True/False] [entrenamiento]")
+    sys.exit(1)
+
+PANORAMACNN = str_to_bool(sys.argv[1])
+Explainable = str_to_bool(sys.argv[2])
+entrenamiento = sys.argv[3]
+
+# Cargar media y std desde fichero
+with open('valores_mean_std.txt', 'r') as f:
+    lines = f.readlines()
+mean_train = [float(value) for value in lines[0].strip().split(',')]
+std_train = [float(value) for value in lines[1].strip().split(',')]
+
+# Configuración de las imágenes
 image_size = (36,108)
 image_dir = './data_img'
 image_files = ['_0_panorama_ext_X.png', '_0_panorama_ext_Y.png', '_0_panorama_ext_Z.png']
@@ -226,46 +282,38 @@ image_files = ['_0_panorama_ext_X.png', '_0_panorama_ext_Y.png', '_0_panorama_ex
 # Configuración del modelo
 if(Explainable):
     path = './entrenamientos/PanoramaCNNEx/' if PANORAMACNN else './entrenamientos/ResnetCNNEx/'
-else:
-    path = './entrenamientos/PanoramaCNN/' if PANORAMACNN else './entrenamientos/ResnetCNN/'
-path += 'entrenamiento_231003_1746'
-
-
-output_dir = './heatmap' + ('PanoramaCNN/' if PANORAMACNN else 'ResnetCNN/')
-if(Explainable):
     output_dir = './heatmap' + ('PanoramaCNNEx/' if PANORAMACNN else 'ResnetCNNEx/')
-else:
-    output_dir = './heatmap' + ('PanoramaCNN/' if PANORAMACNN else 'ResnetCNN/')
-
-if(Explainable):
     model = PanoramaCNNEx() if PANORAMACNN else ResnetCNNEx()
 else:
+    path = './entrenamientos/PanoramaCNN/' if PANORAMACNN else './entrenamientos/ResnetCNN/'
+    output_dir = './heatmap' + ('PanoramaCNN/' if PANORAMACNN else 'ResnetCNN/')
     model = PanoramaCNN() if PANORAMACNN else ResnetCNN()
 
+path += entrenamiento
 
+# Configuración del dispositivo
 print(path)
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(device)
 model.load_state_dict(torch.load(path))  # Load the parameters from the saved state_dict
 model.to(device)  # Move the model to the desired device
 
+# Preprocesado de imágenes
 preprocess = transforms.Compose([
             transforms.ToTensor(),
             transforms.Resize((36,108),antialias=True),
             transforms.Normalize(mean=mean_train, std=std_train)
         ])
 
-model.eval()
-#test(model)
-
+model = model.eval()
 
 df = pd.read_csv('./test.csv', delimiter=',')
 image_dir = df['Sample'].tolist()
 
 new_df = df[['Sample', 'Edad']].copy()
-
 predicted_ages_dict = {}
 saliency_values_dict = {}
+
 for subfolder in image_dir:
 
     print(f"Procesando {subfolder}...")
@@ -285,22 +333,17 @@ for subfolder in image_dir:
 
         images.requires_grad = True
 
-        #apply_saliency(images,model, subfolder, images_wo_p)
+        apply_smoothgrad(images, model, subfolder, images_wo_p)
 
-        #apply_smoothgrad(images, model, subfolder, images_wo_p)
-
-        #apply_integratedgradients(images,model, subfolder, images_wo_p)
+        apply_integratedgradients(images,model, subfolder, images_wo_p)
 
         apply_gradcam(images,model, PANORAMACNN, subfolder, images_wo_p)
 
-'''
+# Resultados a DataFrame
 new_df['Edad_predicha'] = new_df['Sample'].map(predicted_ages_dict)
-new_df['Diferencia'] = (new_df['Edad'] - new_df['Edad_predicha']).abs()
-new_df['Indicador'] = (new_df['Diferencia'] < 1.5).astype(int)
-
 new_df['ponderacionX'] = new_df['Sample'].map(lambda x: saliency_values_dict.get(x, [None, None, None])[0])
 new_df['ponderacionY'] = new_df['Sample'].map(lambda x: saliency_values_dict.get(x, [None, None, None])[1])
 new_df['ponderacionZ'] = new_df['Sample'].map(lambda x: saliency_values_dict.get(x, [None, None, None])[2])
 
+# Guardar resultados
 new_df.to_csv('./predicciones.csv', index=False)
-'''
